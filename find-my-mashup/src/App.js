6efae1e42ce2bfa74a55a2f943b18daf3b47e301
Amelia Rowe <ameliakathleenrowe@gmail.com>
2020-06-27 23:17:59 +1000

    create react app

---
+++ find-my-mashup/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          lol memes 3000
+        </p>
+    </div>
+  );
+}
+
+export default App;
